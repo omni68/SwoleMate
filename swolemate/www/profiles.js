1760d846837b65ecf07fddcc749d43070be54cac
Joshua Jones <joshua.jones@parivedasolutions.com>
2017-01-20 23:00:16 -0600

    upgrading ionic

---
+++ swolemate/www/profiles.js
@@ -0,0 +1,161 @@
+window.localStorage["profiles"] = angular.toJson([
+	{
+		"id": 1,
+		"name": "John Doe",
+		"gender": "Male",
+		"age": 21,
+		"height" :{
+			"ft": 5,
+			"in": 11
+		},
+		"weight": 190,
+		"gyms":[
+			{
+			  "formatted_address": "5645 Beechnut Street, Houston, TX 77096, United States",
+			  "geometry": {
+			    "location": {
+			      "A": 29.687564,
+			      "F": -95.490342
+			    }
+			  },
+			  "icon": "http://maps.gstatic.com/mapfiles/place_api/icons/fitness-71.png",
+			  "id": "df1c6e43925e98f11dd4d0488aa841f853a8a64d",
+			  "name": "Sound Fitness",
+			  "opening_hours": {
+			    "open_now": true,
+			    "weekday_text": []
+			  },
+			  "place_id": "ChIJzXIqNhDCQIYReM_lzdiQKS4",
+			  "reference": "CmRgAAAA4cGai91H4qkOxZuFw8pUeC1UuH9SDZpgIqAX2PLVPrZIaqmErOh-0UkRPTvmMkwZyH6m32e8P7iENirAWYWGOsJhRJei6XWoHECVM9mEPKKDbWYQxNtFKmZ_UsFKNxZbEhBwIb491tlsrDtZuPwr3C9bGhSXAa8_5lMTKTGXwwAYiNkXFPnZOQ",
+			  "types": [
+			    "gym",
+			    "health",
+			    "establishment"
+			  ],
+			  "html_attributions": []
+			},
+			{
+			  "formatted_address": "17099 Walden Road, Montgomery, TX 77356, United States",
+			  "geometry": {
+			    "location": {
+			      "A": 30.386542,
+			      "F": -95.65191800000002
+			    }
+			  },
+			  "icon": "http://maps.gstatic.com/mapfiles/place_api/icons/fitness-71.png",
+			  "id": "7fe14a7c11a871aeea9f4c7dc40c1daafb44c357",
+			  "name": "Spring Fitness",
+			  "opening_hours": {
+			    "open_now": true,
+			    "weekday_text": []
+			  },
+			  "place_id": "ChIJ6ZN5UvIhR4YRMftnxmib9K0",
+			  "reference": "CnRiAAAAoPWFF5WS6h7ib0o7dC405wfRB8ZMyFfDaxmUQIA_xXRKpg51iPwmjMwqo_bAj2szq6U5aSSB2qJGFSzpbXnENgWF2y99STQOnDMLlM_845VF99pKFyxXYh2Dp38EWMquuRZ9tHnoz2s2GnzeqDG7ZxIQPjsn0ZnXZEOL88EiHxgQgRoUyfjbzzW4qNPvtJF4wv4Eu_bhS_M",
+			  "types": [
+			    "gym",
+			    "health",
+			    "establishment"
+			  ],
+			  "html_attributions": []
+			}  
+		],
+		"workoutTypes":[
+			"Strength Training",
+			"Bulking",
+			"Cutting"
+		],
+		"workoutSchedules":[
+			"Early Morning",
+			"Evening"
+		]
+	},
+	{
+		"id": 2,
+		"name": "Jane Smith",
+		"gender": "Female",
+		"age": 26,
+		"height" :{
+			"ft": 5,
+			"in": 8
+		},
+		"weight": 115,
+		"gyms":[
+			{
+			  "formatted_address": "17099 Walden Road, Montgomery, TX 77356, United States",
+			  "geometry": {
+			    "location": {
+			      "A": 30.386542,
+			      "F": -95.65191800000002
+			    }
+			  },
+			  "icon": "http://maps.gstatic.com/mapfiles/place_api/icons/fitness-71.png",
+			  "id": "7fe14a7c11a871aeea9f4c7dc40c1daafb44c357",
+			  "name": "Spring Fitness",
+			  "opening_hours": {
+			    "open_now": true,
+			    "weekday_text": []
+			  },
+			  "place_id": "ChIJ6ZN5UvIhR4YRMftnxmib9K0",
+			  "reference": "CnRiAAAAoPWFF5WS6h7ib0o7dC405wfRB8ZMyFfDaxmUQIA_xXRKpg51iPwmjMwqo_bAj2szq6U5aSSB2qJGFSzpbXnENgWF2y99STQOnDMLlM_845VF99pKFyxXYh2Dp38EWMquuRZ9tHnoz2s2GnzeqDG7ZxIQPjsn0ZnXZEOL88EiHxgQgRoUyfjbzzW4qNPvtJF4wv4Eu_bhS_M",
+			  "types": [
+			    "gym",
+			    "health",
+			    "establishment"
+			  ],
+			  "html_attributions": []
+			}  
+		],
+		"workoutTypes":[
+			"Strength Training"
+		],
+		"workoutSchedules":[
+			"Early Morning",
+			"Evening"
+		]
+	},
+	{
+		"id": 3,
+		"name": "Pete Jones",
+		"gender": "Male",
+		"age": 51,
+		"height" :{
+			"ft": 5,
+			"in": 9
+		},
+		"weight": 210,
+		"gyms":[
+			{
+			  "formatted_address": "17099 Walden Road, Montgomery, TX 77356, United States",
+			  "geometry": {
+			    "location": {
+			      "A": 30.386542,
+			      "F": -95.65191800000002
+			    }
+			  },
+			  "icon": "http://maps.gstatic.com/mapfiles/place_api/icons/fitness-71.png",
+			  "id": "7fe14a7c11a871aeea9f4c7dc40c1daafb44c357",
+			  "name": "Spring Fitness",
+			  "opening_hours": {
+			    "open_now": true,
+			    "weekday_text": []
+			  },
+			  "place_id": "ChIJ6ZN5UvIhR4YRMftnxmib9K0",
+			  "reference": "CnRiAAAAoPWFF5WS6h7ib0o7dC405wfRB8ZMyFfDaxmUQIA_xXRKpg51iPwmjMwqo_bAj2szq6U5aSSB2qJGFSzpbXnENgWF2y99STQOnDMLlM_845VF99pKFyxXYh2Dp38EWMquuRZ9tHnoz2s2GnzeqDG7ZxIQPjsn0ZnXZEOL88EiHxgQgRoUyfjbzzW4qNPvtJF4wv4Eu_bhS_M",
+			  "types": [
+			    "gym",
+			    "health",
+			    "establishment"
+			  ],
+			  "html_attributions": []
+			}  
+		],
+		"workoutTypes":[
+			"Strength Training",
+			"Cutting"
+		],
+		"workoutSchedules":[
+			"Anytime"
+		]
+	}
+]);
+
--- types.js
+++
@@ -1,9 +0,0 @@
-window.localStorage["types"] = angular.toJson([
-	{ "name": "Body Building", "selected": false },
-	{ "name": "Circuit Training", "selected": false },
-	{ "name": "Strength Training", "selected": false },
-	{ "name": "Cutting", "selected": false },
-	{ "name": "Bulking", "selected": false },
-	{ "name": "Plyometrics", "selected": false },
-	{ "name": "Power Lifting", "selected": false }
-]);
